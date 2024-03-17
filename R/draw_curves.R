5945073d0cd6ba9e627e69ac05fff9fd3682023a
pedropark99 <pedropark99@gmail.com>
2024-03-17 10:34:43 -0300

    Add filter to remove non-used observations

--- R/draw_curves.R
+++ R/draw_curves.R
@@ -6,10 +6,10 @@ even_spaced_curves <- function(x_start,
                                 min_steps_allowed,
                                 step_length,
                                 d_sep,
-                                flow_field,
-                                flow_field_width) {
+                                flow_field) {
 
-  even_spaced_curves_impl(
+  flow_field_width <- dim(flow_field)[1]
+  df <- even_spaced_curves_impl(
         x_start,
         y_start,
         n_curves,
@@ -20,6 +20,17 @@ even_spaced_curves <- function(x_start,
         flow_field,
         flow_field_width
   )
+
+  df |>
+    tibble::as_tibble() |>
+    dplyr::filter(
+      curve_id > 0,
+      x > 0,
+      y > 0,
+      steps_taken > 0,
+      step_ids > 0,
+      direction_id > 0,
+    )
 }
 
 
@@ -29,11 +40,11 @@ non_overlapping_curves <- function(starting_points,
                                min_steps_allowed,
                                step_length,
                                d_sep,
-                               flow_field,
-                               flow_field_width) {
+                               flow_field) {
 
+  flow_field_width <- dim(flow_field)[1]
   n_curves <- length(starting_points)
-  non_overlapping_curves_impl(
+  df <- non_overlapping_curves_impl(
     starting_points,
     n_curves,
     n_steps,
@@ -43,4 +54,15 @@ non_overlapping_curves <- function(starting_points,
     flow_field,
     flow_field_width
   )
+
+  df |>
+    tibble::as_tibble() |>
+    dplyr::filter(
+      curve_id > 0,
+      x > 0,
+      y > 0,
+      steps_taken > 0,
+      step_ids > 0,
+      direction_id > 0,
+    )
 }
